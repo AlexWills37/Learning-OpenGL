b46b56807c7dde73a867f13301def48afd261b6a
AlexWills37 <alexanderwills37@gmail.com>
2024-02-16 18:36:12 -0500

    Uniforms and VAO

--- LearningOpenGL/res/shaders/Basic.frag
+++ LearningOpenGL/res/shaders/Basic.frag
@@ -4,7 +4,10 @@
 // Layout location defaults to 0 anyway
 layout(location = 0) out vec4 color;
 
+// Uniform variable - way to get data from CPU at draw call to the GPU in shader
+uniform vec4 u_Color;
+
 void main()
 {
-	color = vec4(1.0, 0.0, 0.0, 1.0);
+	color = u_Color;
 }
--- LearningOpenGL/src/Main.cpp
+++ LearningOpenGL/src/Main.cpp
@@ -6,6 +6,14 @@
 #include <string>
 #include <sstream>
 
+// Macro for error checking
+// Wrap OpenGL functions in GLCall() to detect errors from GL Calls
+// In modern versions of OpenGL, there is a debug callback function that can do this
+#define ASSERT(x) if (!(x)) __debugbreak();
+#define GLCall(x) GLClearError();\
+    x;\
+    ASSERT(GLLogCall(#x, __FILE__, __LINE__))
+
 /*
 * Open GL tutorial, following along with The Cherno's OpenGL tutorial series.
 * Tutorial link: https://youtu.be/W3gAzLwfIP0?si=b7KxNgQduwsQ-sth
@@ -15,6 +23,23 @@
 * @date started Feb 15, 2024
 * @date edited Feb 16, 2024
 */
+
+static void GLClearError()
+{
+    while (glGetError() != GL_NO_ERROR);
+}
+
+static bool GLLogCall(const char* function, const char* file, int line)
+{
+    while (GLenum error = glGetError())
+    {
+        std::cout << "[OpenGL Error] (0x" << std::hex << error << "): " <<
+            function << " " << file << ": "<< line << std::endl;
+        return false;
+    }
+    return true;
+}
+
 
 struct ShaderProgramSource {
     std::string VertexSource;
@@ -29,9 +54,10 @@ struct ShaderProgramSource {
  */
 static ShaderProgramSource ParseShader(const std::string& vertexFilepath, const std::string& fragFilepath) 
 {
-    std::stringstream strings[2];
+    std::stringstream strings[2];   // stringstream creates a mutable string to my understanding (it acts as a buffer, like cout)
     
-    std::ifstream stream(vertexFilepath);
+    // Copy files into the strings
+    std::ifstream stream(vertexFilepath);   
     strings[0] << stream.rdbuf();
     stream.close();
 
@@ -39,6 +65,7 @@ static ShaderProgramSource ParseShader(const std::string& vertexFilepath, const 
     strings[1] << stream.rdbuf();
     stream.close();
 
+    // Return a struct with the two strings
     return { strings[0].str(), strings[1].str() };
 }
 
@@ -47,24 +74,27 @@ static ShaderProgramSource ParseShader(const std::string& vertexFilepath, const 
  */
 static unsigned int CompileShader(unsigned int type, const std::string& source)
 {
-    // Create space for the shader
-    unsigned int id = glCreateShader(type);
+    
+    unsigned int id = glCreateShader(type); // Create space for the shader
     const char* src = source.c_str();   // Get char* version of string
     glShaderSource(id, 1, &src, nullptr);   // Create the shader
-    glCompileShader(id);
+    glCompileShader(id);    // Compile shader
 
     // Handle errors
     int result;
-    glGetShaderiv(id, GL_COMPILE_STATUS, &result);
+    glGetShaderiv(id, GL_COMPILE_STATUS, &result);  // Place compile status in 'result'
     if (result == GL_FALSE) {
+        // Get the length of the status
         int length;
         glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
+
+        // Allocate a space for the message and get the actual message
         char* message = (char*)(alloca(length * sizeof(char)));
         glGetShaderInfoLog(id, length, &length, message);
 
         // Print error message
         std::cout << "Failed to compile " << 
-            (type == GL_VERTEX_SHADER ? "vertex" : 
+            (type == GL_VERTEX_SHADER ? "vertex" :  // specify which shader is having the issue
                 (type == GL_FRAGMENT_SHADER ? "fragment" : "other type of")) 
             << " shader." << std::endl;
         std::cout << message << std::endl;
@@ -84,6 +114,7 @@ static unsigned int CreateShader(const std::string& vertexShader, const std::str
     unsigned int vs = CompileShader(GL_VERTEX_SHADER, vertexShader);
     unsigned int fs = CompileShader(GL_FRAGMENT_SHADER, fragmentShader);
 
+    // Create the shader program
     glAttachShader(program, vs);
     glAttachShader(program, fs);
     glLinkProgram(program);
@@ -94,7 +125,6 @@ static unsigned int CreateShader(const std::string& vertexShader, const std::str
     glDeleteShader(fs);
 
     return program;
-
 }
 
 
@@ -118,6 +148,8 @@ int main(void)
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
+    glfwSwapInterval(1);    // Synchronize frame updates with V sync
+
     /* Initialize GLEW */
     GLenum err = glewInit();
     if (GLEW_OK != err)
@@ -129,20 +161,36 @@ int main(void)
 
     std::cout << "Using OpenGL: " << glGetString(GL_VERSION) << std::endl;
 
-    /* Initialize scene */
 
-    // Triangle verticies
-    float positions[6] = {
-        -0.5f, -0.5f, 
-         0.0f,  0.5f, 
-         0.5f, -0.5f
+
+
+    /* ~~~~~~~~~~ Initialize scene ~~~~~~~~~~ */
+
+    // verticies
+    float positions[] = {
+        -0.5f, -0.5f,   // 0 
+         0.5f, -0.5f,   // 1
+         0.5f,  0.5f,   // 2
+        -0.5f,  0.5f,   // 3
     };
+    
+    // Index buffer to avoid duplicating verticies
+    unsigned int indices[]{
+        0, 1, 2,
+        2, 3, 0
+    };   // Now to send this to the GUP, we need a vertex buffer
+
+
+    // Before making buffers, lets set up the VAO (Vertex Array Object)
+    unsigned int vao;
+    GLCall(glGenVertexArrays(1, &vao));
+    GLCall(glBindVertexArray(vao));
+
 
     // Create buffer
     unsigned int buffer;
     glGenBuffers(1, &buffer);
     glBindBuffer(GL_ARRAY_BUFFER, buffer);  // 'Select' the buffer to modify
-    glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);    // Add data
 
     // index: This attribute will be index 0
     // size: The attribute has 2 components (x, y)
@@ -152,13 +200,50 @@ int main(void)
     // pointer: now that we are in the vertex, we offset 0 bytes to get to the beginning of this attribute
     glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), 0);
     glEnableVertexAttribArray(0);   // Enable this attribute
+    glBufferData(GL_ARRAY_BUFFER, 4 * 2 * sizeof(float), positions, GL_STATIC_DRAW);    // Add data// Create buffer
+
+    // Index buffer object (ibo)
+    unsigned int ibo;
+    glGenBuffers(1, &ibo);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);  // 'Select' the buffer to modify
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(unsigned int), indices, GL_STATIC_DRAW);    // Add data
 
     // Parse the shader
     ShaderProgramSource sources = ParseShader("res/shaders/Basic.vert", "res/shaders/Basic.frag");
-    
-
     unsigned int shader = CreateShader(sources.VertexSource, sources.FragmentSource);
     glUseProgram(shader);
+    
+    // Now that a shader is bound, we can add uniform variables to send data to shaders
+    int location = glGetUniformLocation(shader, "u_Color"); // In modern OpenGL, we can assign the location, but this works too
+    ASSERT(location != -1); // happens when spelling is wrong, or when uniform is unused + erased by compilation
+
+    // Set up animation of color
+    float r = 0.0f;
+    float increment = 0.05f;
+    
+
+    /* Why use Vertex Arrays?
+
+    // Unlink everything!! :(
+    GLCall(glUseProgram(0));
+    GLCall(glBindBuffer(GL_ARRAY_BUFFER, 0));
+    GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0));
+    
+
+    // Rebind everything! :) this would happen every frame if we had multiple buffers/objects to render
+    GLCall(glUseProgram(shader));
+    GLCall(glBindBuffer(GL_ARRAY_BUFFER, buffer));
+    GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo));
+
+    // These 2 lines! the layout might have changed for different draws
+    GLCall(glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), 0));
+    GLCall(glEnableVertexAttribArray(0));   
+    *
+    * Those last 2 lines are why we use vertex arrays; it binds the attribute layout to the vertex buffer
+    * Also, in the core OpenGL, Vertex arrays are a requirement
+    */
+
+
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
@@ -166,8 +251,23 @@ int main(void)
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
+        GLCall(glUniform4f(location, r, 0.6f, 0.9f, 1.0f));
+
+        // animation
+        if (r > 1.0f)
+            increment = -0.05f;
+        else if (r < 0.05f)
+            increment = 0.05f;
+        r += increment;
+
         // Draw buffer
-        glDrawArrays(GL_TRIANGLES, 0, 3);   // This method does not use an index buffer
+        // glDrawArrays(GL_TRIANGLES, 0, 6);   // This method does not use an index buffer
+
+        // With an index buffer, we are drawing 6 indices. Since the buffer is currently bound, we do not need a pointer to it
+        // The vertices are bound to the ARRAY_BUFFER slot
+        // The indices are bound to the ELEMENT_ARRAY_BUFFER slot
+        GLCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr));
+
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
